4a5d8e1c25ef4bedbbb64994d23a4f6b340e45ba
TopXeQ <topxeq@163.com>
2020-04-28 10:36:56 +0800

    2020042801

--- tk.go
+++ tk.go
@@ -5572,6 +5572,28 @@ func CreateSimpleEvent(typeA string, valueA string) *SimpleEvent {
 
 // Misc Related
 
+// ParseHexColor inspired by gg
+func ParseHexColor(x string) (r, g, b, a int) {
+	x = strings.TrimPrefix(x, "#")
+	a = 255
+	if len(x) == 3 {
+		format := "%1x%1x%1x"
+		fmt.Sscanf(x, format, &r, &g, &b)
+		r |= r << 4
+		g |= g << 4
+		b |= b << 4
+	}
+	if len(x) == 6 {
+		format := "%02x%02x%02x"
+		fmt.Sscanf(x, format, &r, &g, &b)
+	}
+	if len(x) == 8 {
+		format := "%02x%02x%02x%02x"
+		fmt.Sscanf(x, format, &r, &g, &b, &a)
+	}
+	return
+}
+
 // DeepClone deep copies original and returns the copy as an interface.
 func DeepClone(original interface{}) (copy interface{}) {
 	if original == nil {
